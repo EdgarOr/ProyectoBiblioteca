d58978d9c1129596c9ce649fe19239d53ccffbec
EdgarOr <ordo_1994@hotmail.com>
2016-05-29 19:45:15 -0500

    Modal Prestamo

---
+++ includes/modals/modalPrestamo.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="modalPrestamo">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        
+         <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+          <legend>Préstamo</legend>
+        </div>
+        
+        <div class="modal-body">
+          <div class="form-group">
+              <label for="">Introduzca el número de ejemplares:</label>
+              
+          </div>
+        </div>
+        
+        <div class="modal-footer">
+        
+          <a href="#" data-dismiss="modal" class="btn btn-danger">Cancelar</a>
+          <a  class="btn btn-success">Aceptar</a>
+        </div>
+      </div>
+    </div>
+</div> 
